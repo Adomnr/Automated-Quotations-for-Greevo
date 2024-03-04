4d1bf1f6e2d2d696a9838a73c8078230b3d91961
Memory <59786843+Adomnr@users.noreply.github.com>
2024-03-04 14:45:52 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from readwritegooglesheets import *
+import pandas as pd
+
+print(Hybrid_Offgrid_Inverters['A5'].value)
